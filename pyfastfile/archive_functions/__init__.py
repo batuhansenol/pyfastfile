ab62d02e5df70362cc3a8905fc7e57c88c9f3247
Batuhan Şenol <batuhansenol83@gmail.com>
2026-06-21 20:02:19 +0300

    bump version to 0.6.1post1

--- pyfastfile/archive_functions/__init__.py
+++ pyfastfile/archive_functions/__init__.py
@@ -4,5 +4,8 @@ from .mod_zip_create import zip_create
 from .mod_zip_extract import zip_extract
 from .mod_zip_is_zipfile import zip_is_zipfile
 from .mod_zip_list import zip_list
+from .mod_tar_create import tar_create
+from .mod_tar_extract import tar_extract
+from .mod_tar_list import tar_list
 
 
